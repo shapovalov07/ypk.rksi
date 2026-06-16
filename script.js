22805fd1410f08c5620087e112c88be1b31fedb3
Nikita <nikitashapovalov2007@gmail.com>
2026-06-16 19:35:32 +0300

    Remove footer archive download

--- script.js
+++ script.js
@@ -16,24 +16,3 @@ if (menuButton && siteNav) {
     }
   });
 }
-
-const footerDownload = document.querySelector(".footer-download");
-
-if (footerDownload instanceof HTMLButtonElement) {
-  footerDownload.addEventListener("click", () => {
-    const downloadUrl = footerDownload.dataset.downloadUrl;
-    const downloadName = footerDownload.dataset.downloadName || "";
-
-    if (!downloadUrl) {
-      return;
-    }
-
-    const link = document.createElement("a");
-    link.href = downloadUrl;
-    link.download = downloadName;
-    link.style.display = "none";
-    document.body.append(link);
-    link.click();
-    link.remove();
-  });
-}
